2f2171786fb9b18995f50b036f8b0c8dae36ba94
Enrique Portillo <167843093+KALACAMAN@users.noreply.github.com>
2024-10-31 15:32:21 -0600

    Proyecto final Fullstack

---
+++ proyectoFinalFSInte/src/main/java/com/example/proyectoFinalFSInte/ProyectoFinalFsInteApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoFinalFSInte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalFsInteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalFsInteApplication.class, args);
+	}
+
+}
